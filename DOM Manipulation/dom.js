a956b7c439125d143ad5a87449f10d21a730340f
coldcoffeee <mhtgupta.281@gmail.com>
2023-04-08 00:11:05 +0530

    "Added index.html and dom.js, did some DOM manipulation"

---
+++ DOM Manipulation/dom.js
@@ -0,0 +1,5 @@
+const header = document.getElementById("main-header");
+header.style.borderBottom = "5px solid black";
+document.title = "Changed the title from JS";
+document.querySelector(".title").style.color = "green";
+document.querySelector(".title").style.fontWeight = "bold";
