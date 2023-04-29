037001a3903d7aec4eca11504be3b333dbb43a4c
coldcoffeee <mhtgupta.281@gmail.com>
2023-04-30 00:28:47 +0530

    .

--- Chat/app.js
+++ Chat/app.js
@@ -1,68 +1,24 @@
-const express = require("express");
-const bp = require("body-parser");
-const fs = require("fs");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.use(bp.urlencoded({ extended: false }));
-app.use(bp.json());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.post("/", (req, res, next) => {
-  let chats = fs.readFileSync("./chat.txt").toString();
-  if (!chats) chats = "Messages appear here:<br>";
-  if (req.body.message) {
-    chats += req.body.user + ": " + req.body.message + "<br>";
-    fs.writeFileSync("./chat.txt", chats);
-    return res.redirect("/");
-    // page reload karne pe previous method se call hota hai page with previous form data
-  } else {
-    console.log(req.body);
-  }
-  res.send(`
-    <html>
-        <head><title>Chat</title></head>
-            <body>
-                <div>
-                    ${chats}
-                </div>
-                <form method = "POST" action="/">
-                    <input type="hidden" name="user" id="user" required>
-                    <input type="text" name="message" placeholder="message" id="message">
-                    <button> Send </button>
-                </form>
-                <script>
-                    document.getElementById("user").value = localStorage.getItem("user");
-                </script>
-            </body>
-        </html>
-    `);
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.get("/login", (req, res, next) => {
-  res.send(`
-        <html>
-            <head><title>Login</title></head>
-            <body>
-                <form method = "POST" action="/">
-                    <input name="user" id="user" required placeholder="username">
-                    <button> Login </button>
-                </form>
-                <script>
-                if(localStorage.getItem("user")) {
-                    document.querySelector("input").value = localStorage.getItem("user");
-                    document.querySelector("form").submit();
-                }
-                    document.querySelector("button").addEventListener("click",()=>{
-                        localStorage.setItem("user", document.getElementById("user").value);
-                    }); 
-                </script>
-            </body>
-        </html>
-    `);
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res, next) => {
-  res.redirect("/login");
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(8080);
+app.use(errorController.get404);
+
+app.listen(3000);
