fee5acd0b9b5e5feb519860d2841f02fccd6baa5
coldcoffeee <mhtgupta.281@gmail.com>
2023-05-04 19:18:16 +0530

    added many to many rel

--- Shop/models/product.js
+++ Shop/models/product.js
@@ -1,29 +1,27 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const sequelize = require("../util/database");
+const sequelize = require('../util/database');
 
-//create models that will be manages by sequelize
-
-const Product = sequelize.define("product", {
+const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
-    primaryKey: true,
+    primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
-  },
+    allowNull: false
+  }
 });
 
 module.exports = Product;
