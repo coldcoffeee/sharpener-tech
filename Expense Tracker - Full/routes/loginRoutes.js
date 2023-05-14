0d9881fdb8c017168cb7720a2805f42a79a6c96b
coldcoffeee <mhtgupta.281@gmail.com>
2023-05-14 21:59:27 +0530

    Used express session  to authenticate user and added expense-user relation

--- Expense Tracker - Full/routes/loginRoutes.js
+++ Expense Tracker - Full/routes/loginRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const loginController = require("../controllers/loginController");
-
+const { notAuthenticated } = require("../controllers/authenticate");
 const router = express.Router();
 
-router.post("/", loginController);
+router.get("/", notAuthenticated, loginController.loginPage);
+router.post("/", notAuthenticated, loginController.authorize);
+router.get("/logout", loginController.logout);
 
 module.exports = router;
